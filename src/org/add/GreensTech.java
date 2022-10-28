bd8b5c1cccf31cffda795f68f168a96eef330df8
Selvavigneshv <selvavigneshmech118@gmail.com>
2022-10-28 20:52:16 +0530

    My Task

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,27 @@
+package org.add;
+
+public class GreensTech {
+	public void greensOmr() {
+System.out.println("Green tech OMR Address:xxxx");
+	}
+	public void greensAdayar() {
+System.out.println("Greens tech Adayar Address:yyyy");
+	}
+	public void greensTambaram() {
+System.out.println("Greens tech Tambaram Address:zzzz");
+	}
+	public void greensVelacherry() {
+System.out.println("Greens tech Velacherry Address: wwww");
+	}
+public void greensAnnaNagar() {
+System.out.println("Greens tech AnnaNagar Address:aaaa");
+}
+public static void main(String[] args) {
+	GreensTech a = new GreensTech();
+	a.greensOmr();
+	a.greensAdayar();
+	a.greensTambaram();
+	a.greensVelacherry();
+	a.greensAnnaNagar();
+}
+}
